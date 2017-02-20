78ce024503716483157223786f1ee0fc2d141376
Varad <py.varad@gmail.com>
2017-02-20 06:13:46 +0530

    updated puppet file

--- jubliant-meme/puppet/modules/pip/manifests/init.pp
+++ jubliant-meme/puppet/modules/pip/manifests/init.pp
@@ -1,8 +1,8 @@
 class pip {
     package { 'python-pip':
         ensure => latest,
+        require => Package['epel-release'],
     }
-
     exec { 'Install-pip-requirements':
         command => 'pip install -r requirements.txt',
         cwd => '/webapp/FlaskApp/',
