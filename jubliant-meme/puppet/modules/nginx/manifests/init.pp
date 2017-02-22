c114c0ad30ab7878f483dbd9cd154f9ab8fd3d9c
Varad <py.varad@gmail.com>
2017-02-22 21:45:22 +0530

    updated the puppet script

--- jubliant-meme/puppet/modules/nginx/manifests/init.pp
+++ jubliant-meme/puppet/modules/nginx/manifests/init.pp
@@ -14,4 +14,27 @@ class nginx {
         enable => true,
         require => Package['nginx'],
     }
+    service {'nginx_stopped':
+        ensure => stopped,
+        require => [Package['nginx'], Service['nginx']],
+    }
+    file {'/etc/nginx/sites-available':
+        ensure => directory,
+        require => Package['nginx'],
+    }
+    file {'/etc/nginx/sites-enabled':
+        ensure => directory,
+        require => File['/etc/nginx/sites-available'],
+    }
+    file {'/etc/nginx/sites-available/flaskapp.conf':
+        ensure => present,
+        source => "puppet:///modules/nginx/flaskapp.conf",
+        notify => Service['nginx'],
+        require => File['/etc/nginx/sites-enabled'],
+    }
+    file { '/etc/nginx/sites-enabled/flaskapp.conf':
+        ensure => 'link',
+        target => '/etc/nginx/sites-available/flaskapp.conf',
+        require => File['/etc/nginx/sites-available/flaskapp.conf'],
+    }
 }
